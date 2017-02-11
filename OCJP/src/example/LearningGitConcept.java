33211eed34e39d699a7493b59ea76653a17f28d0
ketan Bhavsar <ketan@DESKTOP-NVRKIE8>
2017-02-11 23:44:30 +0530

    Second change to Git to check

--- OCJP/src/example/LearningGitConcept.java
+++ OCJP/src/example/LearningGitConcept.java
@@ -4,5 +4,7 @@ public class LearningGitConcept {
 
 	public static void main(String[] args) {
 		System.out.println("This is first commit to Git.");
+		
+		System.out.println("This is the second change I am doing to the git repository.");
 	}
 }
