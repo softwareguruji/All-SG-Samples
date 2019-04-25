e66d0c86036e2a15e03efe9fb822aed1073371cd
ketan Bhavsar <ketan@DESKTOP-NVRKIE8>
2019-04-25 11:41:52 +0530

    Sample

--- OCJP/src/techgig/codegladiator/practice/ChessKingHorseProblem.java
+++ OCJP/src/techgig/codegladiator/practice/ChessKingHorseProblem.java
@@ -1,4 +1,3 @@
-//https://www.techgig.com/codegladiators/question/ODk3MEAjJEAjJDE3Nzk2OTlAIyRAIyQ0NjM2OTE5QCMkQCMkMTQ5NTA3OTkyMQ==/1
 package techgig.codegladiator.practice;
 
 public class ChessKingHorseProblem {
@@ -13,226 +12,86 @@ public class ChessKingHorseProblem {
 		int y2 = 5;*/
 		
 		//Case 2
-		int x1 = 7;
-		int y1 = 7;
-		int x2 = 2;
-		int y2 = 2;
+		int x1 = 6;
+		int y1 = 6;
+		int x2 = 5;
+		int y2 = 5;
 		System.out.println(getStepCount(x1, y1, x2, y2));
 	}
 	
 	public static int getStepCount(int x1, int y1, int x2, int y2){
 		
-		//System.out.println("("+x1+","+y1+")");
+		System.out.println("("+x1+","+y1+")");
 		int moveCounter = 0;
 		while(!(x1 == x2 && y1 == y2)){
 			try{
 				if(x1 < x2){
 					if(y1 < y2){
-						if(x2 - x1 == 1 && y2 - y1 == 1){
-							int temp[] = moveTL(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}else if(x2 - x1 == 2 && y2 - y1 == 1){
-							int temp[] = moveRU(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}else if(x2 - x1 == 3 && y2 - y1 == 1){
-							int temp[] = moveRD(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}else if(x2 - x1 == 1 && y2 - y1 == 2){
-							int temp[] = moveTR(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}else{
-							int temp[] = moveRU(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}
-					}else if(y1 > y2){
-						if(x2 - x1 == 1 && y1 - y2 == 1){
-							int temp[] = moveBL(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}else if(x2 - x1 == 2 && y1 - y2 == 1){
-							int temp[] = moveRD(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}else if(x2 - x1 == 3 && y1 - y2 == 1){
-							int temp[] = moveRU(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}else if(x2 - x1 == 1 && y1 - y2 == 2){
-							int temp[] = moveBR(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}else{
-							int temp[] = moveRD(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}
-					}else if(y1 == y2){
-						if(x2 - x1 == 1){
-							int temp[] = moveLD(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}else if(x2 - x1 == 2){
-							int temp[] = moveBR(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}else if(x2 - x1 == 3){
-							int temp[] = moveRD(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}else{
-							int temp[] = moveRD(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}
+						int temp[] = moveRU(x1, y1);
+						x1 = temp[0];
+						y1 = temp[1];
+						moveCounter++;
+					}else{
+						int temp[] = moveRD(x1, y1);
+						x1 = temp[0];
+						y1 = temp[1];
+						moveCounter++;
 					}
 				}else if(x1 == x2){
+					if(y1 < y2){
+						if(y2 - y1 == 2){
+							int temp[] = moveRU(x1, y1);
+							x1 = temp[0];
+							y1 = temp[1];
+							moveCounter++;							
+						}else{
+							int temp[] = moveRU(x1, y1);
+							x1 = temp[0];
+							y1 = temp[1];
+							moveCounter++;
+						}
+					}else{
+						int temp[] = moveRD(x1, y1);
+						x1 = temp[0];
+						y1 = temp[1];
+						moveCounter++;
+					}
+				}else if(x1 > x2){
 					if(y1 < y2){
 						if(y2 - y1 == 1){
 							int temp[] = moveLU(x1, y1);
 							x1 = temp[0];
 							y1 = temp[1];
 							moveCounter++;							
-						}else if(y2 - y1 == 2){
-							int temp[] = moveLU(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;							
-						}else if(y2 - y1 == 3){
-							int temp[] = moveTL(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;							
 						}else{
-							int temp[] = moveTL(x1, y1);
+							int temp[] = moveRU(x1, y1);
 							x1 = temp[0];
 							y1 = temp[1];
 							moveCounter++;
 						}
-					}else if(y1 > y2){
+					}if(y1 > y2){
 						if(y1 - y2 == 1){
-							int temp[] = moveLD(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;							
-						}else if(y1 - y2 == 2){
-							int temp[] = moveLD(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;							
-						}else if(y1 - y2 == 3){
-							int temp[] = moveBL(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;							
-						}else{
-							int temp[] = moveBL(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}
-					}
-					//No any case of x1 == x2 along with y1 == y2. If it is problem already resolved.
-				}else if(x1 > x2){
-					if(y1 < y2){
-						if(x1 - x2 == 1 && y2 - y1 == 1){
-							int temp[] = moveTR(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}else if(x1 - x2 == 2 && y2 - y1 == 1){
-							int temp[] = moveLU(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}else if(x1 - x2 == 3 && y2 - y1 == 1){
-							int temp[] = moveLD(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}else if(x1 - x2 == 1 && y2 - y1 == 2){
-							int temp[] = moveTL(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}else{
-							int temp[] = moveLU(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}
-					}else if(y1 > y2){
-						if(x1 - x2 == 1 && y1 - y2 == 1){
 							int temp[] = moveBR(x1, y1);
 							x1 = temp[0];
 							y1 = temp[1];
-							moveCounter++;
-						}else if(x1 - x2 == 2 && y1 - y2 == 1){
-							int temp[] = moveLD(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}else if(x1 - x2 == 3 && y1 - y2 == 1){
-							int temp[] = moveLU(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}else if(x1 - x2 == 1 && y1 - y2 == 2){
-							int temp[] = moveBL(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
+							moveCounter++;							
 						}else{
-							int temp[] = moveLD(x1, y1);
+							int temp[] = moveRU(x1, y1);
 							x1 = temp[0];
 							y1 = temp[1];
 							moveCounter++;
 						}
-					}else if(y1 == y2){
-						if(x1 - x2 == 1){
-							int temp[] = moveRD(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}else if(x1 - x2 == 2){
-							int temp[] = moveBL(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}else if(x1 - x2 == 3){
-							int temp[] = moveLD(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}else{
-							int temp[] = moveLD(x1, y1);
-							x1 = temp[0];
-							y1 = temp[1];
-							moveCounter++;
-						}
+					}else{
+						int temp[] = moveRD(x1, y1);
+						x1 = temp[0];
+						y1 = temp[1];
+						moveCounter++;
 					}
 				}
 			}catch(Exception e){
-				//don't do any thing.
-				//partial solution didn't considered border while chess board.
+				//Need to work here.
+				e.printStackTrace();
+				break;
 			}
 		}
 		
@@ -240,7 +99,7 @@ public class ChessKingHorseProblem {
 	}
 	
 	public static void printMove(int[] x){
-		//System.out.println("("+x[0]+","+x[1]+")");
+		System.out.println("("+x[0]+","+x[1]+")");
 	}
 	
 	//Move Top Left
@@ -250,7 +109,7 @@ public class ChessKingHorseProblem {
 		newX[1] = y+2;
 		
 		if(newX[0] < 1 || newX[0] > 8 || newX[1] < 1 || newX[1] > 8)
-			throw new Exception("Invalid Move:TL");
+			throw new Exception("Invalid Move");
 		
 		printMove(newX);
 		return newX;
@@ -263,7 +122,7 @@ public class ChessKingHorseProblem {
 		newX[1] = y+2;
 		
 		if(newX[0] < 1 || newX[0] > 8 || newX[1] < 1 || newX[1] > 8)
-			throw new Exception("Invalid Move:TR");
+			throw new Exception("Invalid Move");
 		
 		printMove(newX);
 		return newX;
@@ -276,7 +135,7 @@ public class ChessKingHorseProblem {
 		newX[1] = y-2;
 		
 		if(newX[0] < 1 || newX[0] > 8 || newX[1] < 1 || newX[1] > 8)
-			throw new Exception("Invalid Move:BL");
+			throw new Exception("Invalid Move");
 		
 		printMove(newX);
 		return newX;
@@ -289,7 +148,7 @@ public class ChessKingHorseProblem {
 		newX[1] = y-2;
 		
 		if(newX[0] < 1 || newX[0] > 8 || newX[1] < 1 || newX[1] > 8)
-			throw new Exception("Invalid Move:BR");
+			throw new Exception("Invalid Move");
 		
 		printMove(newX);
 		return newX;
@@ -302,7 +161,7 @@ public class ChessKingHorseProblem {
 		newX[1] = y+1;
 		
 		if(newX[0] < 1 || newX[0] > 8 || newX[1] < 1 || newX[1] > 8)
-			throw new Exception("Invalid Move:LU");
+			throw new Exception("Invalid Move");
 		
 		printMove(newX);
 		return newX;
@@ -315,7 +174,7 @@ public class ChessKingHorseProblem {
 		newX[1] = y-1;
 		
 		if(newX[0] < 1 || newX[0] > 8 || newX[1] < 1 || newX[1] > 8)
-			throw new Exception("Invalid Move:LD");
+			throw new Exception("Invalid Move");
 		
 		printMove(newX);
 		return newX;
@@ -328,7 +187,7 @@ public class ChessKingHorseProblem {
 		newX[1] = y+1;
 		
 		if(newX[0] < 1 || newX[0] > 8 || newX[1] < 1 || newX[1] > 8)
-			throw new Exception("Invalid Move:RU");
+			throw new Exception("Invalid Move");
 		
 		printMove(newX);
 		return newX;
@@ -341,7 +200,7 @@ public class ChessKingHorseProblem {
 		newX[1] = y-1;
 		
 		if(newX[0] < 1 || newX[0] > 8 || newX[1] < 1 || newX[1] > 8)
-			throw new Exception("Invalid Move:RD");
+			throw new Exception("Invalid Move");
 		
 		printMove(newX);
 		return newX;
